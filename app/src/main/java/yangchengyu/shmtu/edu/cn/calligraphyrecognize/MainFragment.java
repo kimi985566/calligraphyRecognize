fda1d33e7ef9a28122954542625f3f815c6d7900
kimi985566 <chengyu_yang@outlook.com>
2018-01-26 09:33:41 +0800

    my application

--- app/src/main/java/yangchengyu/shmtu/edu/cn/calligraphyrecognize/MainFragment.java
+++ app/src/main/java/yangchengyu/shmtu/edu/cn/calligraphyrecognize/MainFragment.java
@@ -9,6 +9,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.FrameLayout;
 
 import java.util.ArrayList;
@@ -41,6 +43,7 @@ public class MainFragment extends Fragment {
             return view;
         } else {
             View view = inflater.inflate(R.layout.fragment_setting, container, false);
+            initSetting(view);
             return view;
         }
     }
@@ -62,8 +65,31 @@ public class MainFragment extends Fragment {
     }
 
     private void initMainCamera(View view) {
-
+        //to be done
     }
 
+    private void initSetting(View view) {
+        //to be done
+    }
 
+    public void refresh() {
+        if (getArguments().getInt("index", 0) > 0 && mRecyclerView != null) {
+            mRecyclerView.smoothScrollToPosition(0);
+        }
+    }
+
+    public void willBeDisplayed() {
+        // Do what you want here, for example animate the content
+        if (mFragmentContainer != null) {
+            Animation fadeIn = AnimationUtils.loadAnimation(getActivity(), R.anim.fade_in);
+            mFragmentContainer.startAnimation(fadeIn);
+        }
+    }
+
+    public void willBeHidden() {
+        if (mFragmentContainer != null) {
+            Animation fadeOut = AnimationUtils.loadAnimation(getActivity(), R.anim.fade_out);
+            mFragmentContainer.startAnimation(fadeOut);
+        }
+    }
 }
