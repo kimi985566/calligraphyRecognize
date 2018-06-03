456f5524628a574ec442d5f3db4296325d56aecb
Chengyu Yang <chengyu_yang@outlook.com>
2018-06-03 22:00:14 +0800

    extension

---
+++ app/src/main/java/yangchengyu/shmtu/edu/cn/calligraphyrecognize/utils/CommonExt.kt
@@ -0,0 +1,26 @@
+package yangchengyu.shmtu.edu.cn.calligraphyrecognize.utils
+
+import android.view.View
+
+/*
+    扩展视图可见性
+ */
+fun View.setVisible(visible: Boolean) {
+    this.visibility = if (visible) View.VISIBLE else View.GONE
+}
+
+/*
+    扩展点击事件
+ */
+fun View.onClick(listener: View.OnClickListener): View {
+    setOnClickListener(listener)
+    return this
+}
+
+/*
+    扩展点击事件，参数为方法
+ */
+fun View.onClick(method: () -> Unit): View {
+    setOnClickListener { method() }
+    return this
+}
